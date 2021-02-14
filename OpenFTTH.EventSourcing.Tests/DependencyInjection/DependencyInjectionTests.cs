7bafc19c34b39ad3c68921705572c81b45191c69
Jesper Ladegaard <jesper@dax.dk>
2021-02-14 22:25:38 +0100

    fix unit test

--- OpenFTTH.EventSourcing.Tests/DependencyInjection/DependencyInjectionTests.cs
+++ OpenFTTH.EventSourcing.Tests/DependencyInjection/DependencyInjectionTests.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using FluentAssertions;
+using System.Threading;
 
 namespace OpenFTTH.EventSourcing.Tests
 {
@@ -15,16 +16,17 @@ namespace OpenFTTH.EventSourcing.Tests
         private IServiceProvider _serviceProvider;
         private IEventStore _eventStore;
 
-
-        public DependencyInjectionTests(IServiceProvider serviceCollection, IEventStore eventStore)
+        public DependencyInjectionTests(IServiceProvider serviceProvider, IEventStore eventStore)
         {
-            _serviceProvider = serviceCollection;
+            _serviceProvider = serviceProvider;
+
             _eventStore = eventStore;
         }
 
         [Fact]
         public void TestThatProjectionAreAutomaticallyPickedUpInIoCByEventStore()
         {
+            
             // Setup
             var poopProjection = _serviceProvider.GetServices<IProjection>().First(p => p is PoopProjection) as PoopProjection;
 
@@ -34,8 +36,7 @@ namespace OpenFTTH.EventSourcing.Tests
             _eventStore.Aggregates.Store(snoopy);
 
             // Assert
-            poopProjection.PoopReport.Count.Should().Be(1);
-            poopProjection.PoopReport[0].DogName.Should().Be("Snoopy");
+            poopProjection.PoopReport.Find(d => d.DogName == "Snoopy").PoopTotal.Should().Be(200);
         }
 
         [Fact]
@@ -45,13 +46,12 @@ namespace OpenFTTH.EventSourcing.Tests
             var poopProjection = _eventStore.Projections.Get<PoopProjection>();
 
             // Act
-            var snoopy = new DogAggregate(Guid.NewGuid(), "Snoopy");
-            snoopy.Poop(200);
+            var snoopy = new DogAggregate(Guid.NewGuid(), "Pluto");
+            snoopy.Poop(500);
             _eventStore.Aggregates.Store(snoopy);
 
             // Assert
-            poopProjection.PoopReport.Count.Should().Be(1);
-            poopProjection.PoopReport[0].DogName.Should().Be("Snoopy");
+            poopProjection.PoopReport.Find(d => d.DogName == "Pluto").PoopTotal.Should().Be(500);
         }
     }
 }
