410fde49aad7a8bd40595dd357b9c700e879ddcc
Rune Nielsen <runenielsen@runbox.com>
2022-04-01 09:22:56 +0200

    removes dead usings

--- OpenFTTH.EventSourcing.Tests/Model/DogBorn.cs
+++ OpenFTTH.EventSourcing.Tests/Model/DogBorn.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpenFTTH.EventSourcing.Tests.Model
+﻿namespace OpenFTTH.EventSourcing.Tests.Model
 {
     public class DogBorn
     {
--- OpenFTTH.EventSourcing/InMem/InMemSequenceStore.cs
+++ OpenFTTH.EventSourcing/InMem/InMemSequenceStore.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace OpenFTTH.EventSourcing.InMem
 {
     public class InMemSequenceStore : ISequences
