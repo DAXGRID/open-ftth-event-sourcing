b43e45eac5f3e6cb60d9f187911c0d02fdbc5090
Rune Nielsen <runenielsen@runbox.com>
2023-05-12 14:27:48 +0200

    now exposes event-timestamp in event envelope (#38)

--- OpenFTTH.EventSourcing/EventEnvelope.cs
+++ OpenFTTH.EventSourcing/EventEnvelope.cs
@@ -8,15 +8,22 @@ namespace OpenFTTH.EventSourcing
         public Guid EventId { get; }
         public long Version { get; }
         public long GlobalVersion { get; }
-        public DateTime EventTimeStamp { get; init; }
+        public DateTime EventTimestamp { get; init; }
         public object Data { get; }
 
-        public EventEnvelope(Guid streamId, Guid eventId, long version, long globalVersion, DateTime eventTimeStamp, object data)
+        public EventEnvelope(
+            Guid streamId,
+            Guid eventId,
+            long version,
+            long globalVersion,
+            DateTime eventTimestamp,
+            object data)
         {
             StreamId = streamId;
             EventId = eventId;
             Version = version;
             GlobalVersion = globalVersion;
+            EventTimestamp = eventTimestamp;
             Data = data;
         }
     }
